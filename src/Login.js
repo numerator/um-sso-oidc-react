9abd1c7e83aab9fbfd06689a362b50d5e7cc37ff
Mark Newman <mwnewman@m-j2lt4gjc00.local>
2024-04-21 16:45:57 -0400

    now checks user profile and displays uniqname

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,34 @@
 import { useAuth } from "oidc-react";
 
+
 export function LoginScreen () {
-
     const auth = useAuth();
+    console.log(auth.userData);
 
-    return (
-        <div>
-            <button onClick={auth.signIn}>sign in</button>
-        </div>
-    )
+
+    if (auth.userData) {
+        return (
+            <div>
+                <p>
+                    Welcome, {auth.userData.profile.preferred_username} !
+                </p>
+                <p>
+                    <button onClick={auth.signOut}>Log out</button>
+
+                </p>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <p>
+                    We are here.
+                </p>
+                <p>
+                    <button onClick={auth.signIn}>Log In</button>
+                </p>
+            </div>
+        )
+    }
 
 }
